435284987aa79e52187bc8551612ce60d2eaed13
badi-al-zaman <badi.al.zaman2446@gmail.com>
2025-10-29 14:43:13 -0700

    feat: replace deprecated memory buffer class to memory class

--- backend/app/controllers/rag_controller_v2.py
+++ backend/app/controllers/rag_controller_v2.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, HTTPException
 from app.services.embedding_service import load_and_chunk_documents_v2
 from app.services.embedding_service import setup_vector_database_v2
 from app.services.retriever_service import search_query_pipline_v2
-from app.services.generator_service import llm_chat_v2
+from app.services.generator_service import llm_chat_v2, ask_agent
 from app.services.rag_service import run_complete_rag_pipeline_v2
 from app.core.config import settings as server_settings
 
@@ -32,24 +32,47 @@ def ask(query: str):
 
 
 from llama_index.core.llms import ChatMessage
-from llama_index.storage.chat_store.postgres import PostgresChatStore
-from llama_index.core.memory import ChatMemoryBuffer
+# from llama_index.core.memory import ChatMemoryBuffer # deprecated
+from llama_index.core.memory import Memory
+from app.core.db import async_engine as pg_engine
 
 
 @router.post("/chat/{session_id}")
-def chat(query: str, session_id: str):
-    chat_store = PostgresChatStore.from_uri(
-        uri=str(server_settings.SQLALCHEMY_DATABASE_URI)
-        # table_name="session"
-    )
-
-    chat_memory = ChatMemoryBuffer.from_defaults(
+async def chat(query: str, session_id: str):
+    chat_memory = Memory.from_defaults(
         token_limit=3000,
-        chat_store=chat_store,
-        chat_store_key=session_id,
+        async_engine=pg_engine,
+        # async_database_uri=str(server_settings.SQLALCHEMY_ASYN_DATABASE_URI),
+        session_id=session_id,
+        table_name="memory_table",
     )
 
-    response = llm_chat_v2(query, chat_memory)
+    response = await llm_chat_v2(query, chat_memory)
+    return {"response": response}
+
+    # messages = chat_store.get_messages("conversation1")
+    # if len(messages) == 0:
+    #     messages = [
+    #         ChatMessage(role="user", content="When did Adam graduate from college?"),
+    #         ChatMessage(role="chatbot", content="1755."),
+    #     ]
+    #  Role should be 'system', 'developer', 'user', 'assistant', 'function', 'tool', 'chatbot' or 'model'
+    #     chat_store.set_messages("conversation1", messages=messages)
+
+    # session = crud.get_full_session(db, session_id)
+
+
+@router.post("/agent/chat/{session_id}")
+async def chat_with_agent(query: str, session_id: str):
+    chat_memory = Memory.from_defaults(
+        token_limit=3000,
+        async_engine=pg_engine,
+        session_id=session_id,
+        table_name="memory_table",
+    )
+    print(chat_memory)
+
+    response = await ask_agent(query, chat_memory)
     return {"response": response}
 
     # messages = chat_store.get_messages("conversation1")
